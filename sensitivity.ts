796970a7af32dbaea9e9796768a5dc0eaddf7dbc
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2023-02-10 22:10:47 +0100

    working

--- sensitivity.ts
+++ sensitivity.ts
@@ -11,29 +11,7 @@ namespace ExpSense {
     let booom = true
     let stop = true
     let onExplosiveStateHandler: (xaxis: number, yaxis: number, shakingvalue: number) => void;
-
-    let reset = (): void => {
-        stop = true
-        basic.showNumber(3, 100)
-        basic.showNumber(2, 100)
-        basic.showNumber(1, 100)
-        let accx = input.acceleration(Dimension.X)
-        let accy = input.acceleration(Dimension.Y)
-        let accs = input.acceleration(Dimension.Strength)
-        for (let i = 0; i < 5; i++) {
-            basic.pause(50)
-            accx += input.acceleration(Dimension.X)
-            accy += input.acceleration(Dimension.Y)
-            accs += input.acceleration(Dimension.Strength)
-        }
-        accxn = Math.idiv(accx, 6)
-        accyn = Math.idiv(accy, 6)
-        accsn = Math.idiv(accs, 6)
-        soundExpression.slide.playUntilDone()
-        clear()
-        stop = false
-        booom = false
-    }
+    let onExplodeHandler: () => void;
 
     let clear = (): void => {
         for (let y = 0; y < 5; y++) {
@@ -57,42 +35,19 @@ namespace ExpSense {
             if (xyzacc > 9) booom = true //shake it, baby
             if (Math.abs(xaxis) > 2 || Math.abs(yaxis) > 2) booom = true //on an inclined surface
 
+            control.inBackground(function () {
+                if (onExplosiveStateHandler) {
+                    onExplosiveStateHandler(xaxis, yaxis, xyzacc);
+                }
+            })
             if (booom && !stop) {
                 stop = true
-                basic.showIcon(IconNames.Sad, 0)
-                soundExpression.sad.playUntilDone()
-            } else {
-                control.inBackground(function () {
-                    if (onExplosiveStateHandler) {
-                        onExplosiveStateHandler(xaxis, yaxis, xyzacc)
-                    }
-                })
+                if (onExplodeHandler) {
+                    onExplodeHandler();
+                }
             }
         }
         basic.pause(100)
-    })
-
-    export function difficultyInc(): number {
-        if (stop) {
-            deltamodifier = Math.constrain(deltamodifier + 0.1, 0.8, 2.8)
-        }
-        return deltamodifier * 10
-    }
-
-    export function difficultyDec(): number {
-        if (stop) {
-            deltamodifier = Math.constrain(deltamodifier - 0.1, 0.8, 2.8)
-        }
-        return deltamodifier * 10
-    }
-
-    export function difficultyGet(): number {
-        return deltamodifier * 10
-    }
-
-    input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-        //radio.sendNumber(deltamodifier * 10 + 12) //submit deltamodifier (difficulty)
-
     })
 
     /**
@@ -102,6 +57,81 @@ namespace ExpSense {
     //% draggableParameters=reporter
     export function onExplosiveState(cb: (xaxis: number, yaxis: number, shakingvalue: number) => void) {
         onExplosiveStateHandler = cb;
+    }
+
+    /**
+    * Registers the code that runs when mc:b has exploded.
+    */
+    //% blockId=ExpSense_explode block="on explode"
+    //% draggableParameters=reporter
+    export function onExplode(cb: () => void) {
+        onExplodeHandler = cb;
+    }
+
+    /**
+    * Start / restart shake monitoring.
+    */
+    //% blockId=ExpSense_restart block="restart shake monitoring"
+    export function restart(): void {
+        stop = true
+        basic.showNumber(3, 100)
+        basic.showNumber(2, 100)
+        basic.showNumber(1, 100)
+        let accx = input.acceleration(Dimension.X)
+        let accy = input.acceleration(Dimension.Y)
+        let accs = input.acceleration(Dimension.Strength)
+        for (let i = 0; i < 5; i++) {
+            basic.pause(50)
+            accx += input.acceleration(Dimension.X)
+            accy += input.acceleration(Dimension.Y)
+            accs += input.acceleration(Dimension.Strength)
+        }
+        accxn = Math.idiv(accx, 6)
+        accyn = Math.idiv(accy, 6)
+        accsn = Math.idiv(accs, 6)
+        soundExpression.slide.playUntilDone()
+        clear()
+        stop = false
+        booom = false
+    }
+    
+    /**
+    * Abort shake monitoring.
+    */  
+    //% blockId=ExpSense_stop block="Abort shake monitoring"
+    export function stopMonitoring(): void {
+        stop = true
+        booom = false
+    }
+
+    /**
+     * Increase the difficulty level
+    **/
+    //% blockId=ExpSense_incdiff block="difficulty increase"
+    export function difficultyInc(): number {
+        if (stop) {
+            deltamodifier = Math.constrain(deltamodifier + 0.1, 0.8, 2.8)
+        }
+        return deltamodifier * 10
+    }
+
+    /**
+    * Reduce the difficulty level
+    **/
+    //% blockId=ExpSense_decdiff block="difficulty reduce"
+    export function difficultyDec(): number {
+        if (stop) {
+            deltamodifier = Math.constrain(deltamodifier - 0.1, 0.8, 2.8)
+        }
+        return deltamodifier * 10
+    }
+
+    /**
+     * Return the difficulty level
+    **/
+    //% blockId=ExpSense_getdiff block="get difficulty"
+    export function difficultyGet(): number {
+        return deltamodifier * 10
     }
 
 }
